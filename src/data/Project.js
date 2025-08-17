70ed91c75a80f9d73bb1c397fa5ca58f3dffbe34
Shahnawaz Hussain <shahnawaz.hussain96508@gmail.com>
2025-08-17 17:18:02 +0530

    Updated Project.js with new project

--- src/data/Project.js
+++ src/data/Project.js
@@ -1,13 +1,16 @@
 const projects = [
     {
-      title: "E-Commerce App",
-      image: "./ShopOn.webp",
-      desc: "I developed the frontend of ShopOn, a modern e-commerce website, using HTML, CSS, and JavaScript. The design includes product listings, navigation menus, and a responsive layout, focusing on clean UI and smooth user experience for online shopping platforms.",
-      stack: ["Frontend: HTML, CSS, JavaScript"],
-      github: "https://github.com/shahnawaz-hussaink/ShopOn",
-      live: "https://shahnawaz-hussaink.github.io/ShopOn/",
+      title: "STUDx - Campus Exchange Hub",
+      image: "./Studx.webp",
+      desc: "STUDx is a full-stack MERN platform designed exclusively for college students to buy, sell, or swap pre-owned items like books, gadgets, and furniture within their campus community. It ensures a safe, student-centric marketplace with categories, best deals, and featured products. The platform has a clean, responsive UI built with React and Tailwind CSS, while the backend is powered by Node.js, Express, and MongoDB for seamless data management.",
+      stack: [
+        "Frontend: React, Tailwind CSS",
+        "Backend: Node.js, Express.js, JWT Authentication",
+        "Database: MongoDB Cloud"
+      ],
+      github: "https://github.com/shahnawaz-hussaink/studx",
+      live: "https://github.com/shahnawaz-hussaink/studx",
     },
-
     {
       title: "Netflix.com Clone",
       image: "./netflix.webp",
@@ -17,7 +20,7 @@ const projects = [
       live: "https://shahnawaz-hussaink.github.io/Netflix/",
     },
     {
-        title: "Simon Says Game",
+        title: "Colored Simon Says Game",
         image: "./SimonSay.webp",
         desc: "I built a classic Simon Says memory game using HTML, CSS, and JavaScript. The game challenges users to recall and repeat increasingly complex sequences of colors with smooth transitions and sound feedback, providing a fun and interactive experience.",
         stack: ["Frontend: HTML, CSS, JavaScript"],
@@ -34,7 +37,7 @@ const projects = [
         "Features: Fetch API, async/await, DOM manipulation",
       ],
       github: "https://github.com/shahnawaz-hussaink/Project-Dog",
-      live: "https://liveproject.com",
+      live: "https://shahnawaz-hussaink.github.io/Project-Dog/",
     },
     {
     title: "Shah's Company Support Chat",
@@ -45,7 +48,7 @@ const projects = [
     live: "https://shahnawaz-hussaink.github.io/ChatbotSupport/"
     },
     {
-        title: "Personal Portfolio (Vanilla JS)",
+        title: "Personal Portfolio (Basic)",
         image: "./OldPortfolio.webp",
         desc: "I created an earlier version of my personal portfolio using HTML, CSS, and vanilla JavaScript. It featured a responsive layout, smooth scrolling, and interactive UI sections like About, Projects, and Contact, designed to showcase my skills and work in a clean and simple format.",
         stack: ["Frontend: HTML, CSS, JavaScript"],
@@ -53,15 +56,22 @@ const projects = [
         live: "https://shahnawaz-hussaink.github.io/Portfolio/"
     },
     {
+      title: "E-Commerce App Homepage",
+      image: "./ShopOn.webp",
+      desc: "I developed the frontend of ShopOn, a modern e-commerce website, using HTML, CSS, and JavaScript. The design includes product listings, navigation menus, and a responsive layout, focusing on clean UI and smooth user experience for online shopping platforms.",
+      stack: ["Frontend: HTML, CSS, JavaScript"],
+      github: "https://github.com/shahnawaz-hussaink/ShopOn",
+      live: "https://shahnawaz-hussaink.github.io/ShopOn/",
+    },
+    {
       title: "Portfolio Website",
       image: "./Portfolio.webp",
       desc: "I designed and developed a fully responsive personal portfolio using React and Tailwind CSS. It showcases my projects, skills, and experience in a clean and accessible layout, with smooth transitions and a modern UI to highlight my web development capabilities.",
       stack: [
-        "Frontend: HTML, CSS, JavaScript, React",
-        "Backend: Node.js, Express, MongoDB",
+        "Frontend: HTML, CSS, JavaScript, React"
       ],
-      github: "https://github.com/yourproject",
-      live: "https://liveproject.com",
+      github: "https://github.com/shahnawaz-hussaink/portfolio2.0",
+      live: "https://portfolio2-0-pi-nine.vercel.app",
     },
   ];
 
