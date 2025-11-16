fad7b125372578f0e240369cdc62883fb6047828
Shahnawaz Hussain <shahnawaz.hussain96508@gmail.com>
2025-11-16 22:09:42 +0530

    Update experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -10,6 +10,19 @@
         "Worked on frontend using React and Tailwind CSS, integrating responsive UI and component logic.",
         "Learned to work in fast-paced environments and present the project to a judging panel."
       ]
-    }
+    },
+    {
+  id: 2,
+  title: "Participant",
+  company: "Pitchcraft",
+  date: "Oct 2025",
+  image: "Pitchcraft.webp",
+  description: [
+    "Developed an AI-powered rental platform to solve uncertainty and trust issues in the rental ecosystem.",
+    "Built using Next.js, FastAPI, Supabase, and blockchain tech with features like AI matching, rental DNA score, and verified listings.",
+    "Collaborated as part of team CAFFeINE-CORRUPT to create a prototype aimed at transforming rental experiences through transparency."
+  ]
+}
+
   ];
   export default experiences;
