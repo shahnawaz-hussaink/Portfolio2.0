9749d275bdf1f1a98e4467cf8a2a1ac504174c0c
shahnawaz-hussaink <shahnawaz.hussain96508@gmail.com>
2026-07-06 14:05:08 +0530

    chore: setup custom github actions deployment for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/Portfolio2.0/' : '/',
   plugins: [react() , tailwindcss(),],
   server: {
     allowedHosts: ['5d01db25e227.ngrok-free.app'],
